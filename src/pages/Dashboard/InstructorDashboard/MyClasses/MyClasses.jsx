089e0740377ff4c67f34083673432d90c4c0e5c6
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-06-10 14:09:42 +0600

    my classes section add

--- src/pages/Dashboard/InstructorDashboard/MyClasses/MyClasses.jsx
+++ src/pages/Dashboard/InstructorDashboard/MyClasses/MyClasses.jsx
@@ -1,16 +1,53 @@
+import { useState } from "react";
 import { useEffect } from "react";
 import { getAllClass } from "../../../../api/class";
 
 const MyClasses = () => {
+  const [myClass, setMyClass] = useState([]);
+
   useEffect(() => {
     getAllClass().then((data) => {
-      console.log(data);
+      setMyClass(data);
     });
-  });
+  }, []);
 
   return (
-    <div>
-      <h3>Hello MY Classes</h3>
+    <div className="my-10">
+      <h1 className="text-3xl font-bold text-center underline mb-5">
+        Review My Classes
+      </h1>
+      <div className="overflow-x-auto">
+        <table className="table">
+          <tbody>
+            {myClass.map((mc, index) => (
+              <tr key={mc._id} className="border">
+                <td>{index + 1}.</td>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img src={mc.image} alt="class img" />
+                      </div>
+                    </div>
+                    <div className="text-xl font-semibold">{mc.name}</div>
+                  </div>
+                </td>
+                <td>Total Enrolled: {0}</td>
+                <td>
+                  <span className="uppercase badge badge-outline text-primary font-semibold">
+                    {mc.status}
+                  </span>
+                </td>
+                <td>
+                  <button className="btn btn-outline btn-ghost btn-xs">
+                    Update
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
